ef1549bd6dd78933fc2071402b68a9b1a3fa3a14
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-03-18 19:08:04 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@
 
 $env = parse_ini_file(__DIR__ . "/../.env");
 var_dump($env);
-$servername = "eteam-manager";
+$servername = "localhost";
 $username = $env['db_user'];
 $password = $env['db_password'];
 $dbname = "eteam_manager";
