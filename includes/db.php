0976988a33cfca4b7207a303b5671f42184c1f33
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-03-18 18:49:29 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,24 +1,24 @@
 <?php
-    /* Codigo para cargar la base de datos */
+/* Codigo para cargar la base de datos */
 
-    $env = parse_ini_file(__DIR__ . "/../.env");
-    $servername = "localhost";
-    $username = $env["db_user"];
-    $password = $env["db_password"];
-    $dbname = "eteam_manager";
+$env = parse_ini_file(__DIR__ . "/../.env");
+$servername = "localhost";
+$username = $env["db_user"];
+$password = $env["db_password"];
+$dbname = "eteam_manager";
 
-    try {
-        $conn = new PDO(
-            "mysql:host=$servername;dbname=$dbname;charset=utf8mb4",
-            $username,
-            $password,
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false
-            ]
-        );
-    } catch (PDOException $e) {
-        die("Error en connectar amb la base de dades.");
-    }
+try {
+    $conn = new PDO(
+        "mysql:host=$servername;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("Error en connectar amb la base de dades.");
+}
 ?>
