4fb5e837d8d9aa7c859a65110b3434fd90ed1092
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-03-19 18:22:43 +0100

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -1,9 +1,7 @@
 <?php
-session_start();
 require_once "auth.php"; // para tener la función logout()
 
 logout(); // limpia la sesión
-var_dump($_SESSION); // debería estar vacío
 // redirige al login
 echo '<script>window.location.href="app.php?view=login";</script>';
 exit;
