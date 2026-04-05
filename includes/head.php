2c56dbd11ae62863959217973da8455684ad22a7
David Perera <davidperera2006@gmail.com>
2026-04-05 14:59:11 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,7 @@
 <?php
 $resolvedTitle = isset($pageTitle) ? $pageTitle . ' | eTeam Manager' : 'eTeam Manager';
+$mainCssPath = __DIR__ . '/../css/main.css';
+$mainCssVersion = is_file($mainCssPath) ? filemtime($mainCssPath) : time();
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -11,6 +13,6 @@ $resolvedTitle = isset($pageTitle) ? $pageTitle . ' | eTeam Manager' : 'eTeam Ma
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css?v=<?php echo (int) $mainCssVersion; ?>">
 </head>
 <body>
