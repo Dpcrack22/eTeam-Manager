91806d853025433501d97f9eb837baf3e81155da
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-05-12 16:42:33 +0200

    Update app.php

--- app.php
+++ app.php
@@ -167,12 +167,7 @@ $appModules = [
 		'title' => 'Registro',
 		'eyebrow' => 'Crear cuenta',
         'description' => 'Crea una cuenta para acceder a eTeam Manager',
-	],
-    'team-profile' => [
-        'label' => 'Perfil de Equipo',
-        'title' => 'Detalle del Equipo',
-        'page' => __DIR__ . '/../pages/team_profile.php', // O la ruta donde esté
-    ],
+	]
 ];
 
 if (!isset($appModules[$view])) {
